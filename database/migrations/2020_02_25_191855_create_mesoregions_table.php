f76b24cd2ee40ed5de996793a9fdd20769bf218c
robson eduardo <robsoneducardo@gmail.com>
2020-02-27 22:36:25 -0300

    fail on inserting foreign keys
    I've tried to put the foreign keys but it doesn't work. :(

--- database/migrations/2020_02_25_191855_create_mesoregions_table.php
+++ database/migrations/2020_02_25_191855_create_mesoregions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Macroregion extends Migration
+class CreateMesoregionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,10 @@ class Macroregion extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('mesoregions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ class Macroregion extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('mesoregions');
     }
 }
