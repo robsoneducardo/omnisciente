4f18cd90aa0e69cd31452828a1a7b64e4c69e53e
robson eduardo <robsoneducardo@gmail.com>
2020-03-22 00:08:04 -0300

    Incluir csv's e migration para macroregioes

---
+++ database/factories/MacroregionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Macroregion;
+use Faker\Generator as Faker;
+
+$factory->define(Macroregion::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/seeds/MacroregionSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+
+class MacroregionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        print ("Inserindo as macroregioes.\n");
+        $csvFile = fopen("database/seeds/macroregions.csv", "r");
+        while (($estado = fgetcsv($csvFile)) !== false){
+            DB::table('macroregions')->insert([
+                'code' => $estado[0],
+                'name' => $estado[1]
+            ]);
+        }
+    }
+}
